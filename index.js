1deea1dcb26ac129f5567046426cb1ea22b692bf
Chopo <104890279+sontopo@users.noreply.github.com>
2023-01-10 15:49:21 +0100

    0.1
    Added bot files

---
+++ index.js
@@ -0,0 +1,22 @@
+const JUGNU = require("./handlers/Client");
+const { TOKEN } = require("./settings/config");
+require("ffmpeg-static");
+const client = new JUGNU();
+module.exports = client;
+
+client.start(TOKEN);
+
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
+
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
+
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
